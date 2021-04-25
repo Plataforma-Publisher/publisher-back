df7d6225600c7caa6dafb8d12655eb827a661093
Guilherme <guifun080808@gmail.com>
2021-04-25 11:47:31 +0100

    up

--- src/main/java/com/platformpublisher/repository/TeamRepository.java
+++ src/main/java/com/platformpublisher/repository/TeamRepository.java
@@ -1,9 +1,10 @@
 package com.platformpublisher.repository;
 
 import com.platformpublisher.model.Team;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-//    List<Team> findByTeamDescriptionStartingWithIgnoreCase(String prefix);
+   // List<Team> findByTeamDescriptionStartingWithIgnoreCase(String prefix);
 }
